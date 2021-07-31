7734cdc225421c0c3f4ed5a2086d5052f8d06cf4
ZRRufino <zrrufino7@gmail.com>
2021-07-31 10:10:44 -0500

    init proyect

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+// components
+import Header from './components/Header';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Hola Mundo</h1>
+			<Header />
+		</div>
+	);
+}
+
+export default App;
